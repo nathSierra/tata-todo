5f0731ff1e7a0741732f6f5ec5478a0508b0076e
Nathan Sierra <arreisnahtan@gmail.com>
2021-08-12 13:02:24 -0400

    feat: conditionally renders login or rregistration

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LoginForm from '../common/components/LoginForm/LoginForm'
 import SignUpForm from '../common/components/LoginForm/SignUpForm'
 
+ enum registration  {
+  SIGNUP = "SIGN_UP",
+  LOGIN = "LOGIN"
+}
+
 export default function Login() {
+  const [View, setView] = useState(registration.LOGIN)
+
+  if (View === registration.LOGIN){
   return (
     <>
    <LoginForm />
-   <SignUpForm />
+    <h3> Not registered? No problem. <button onClick={() => setView(_ => registration.SIGNUP)}>Go to Registration</button>:</h3>
    </>
-  )
+  )}
+  else {
+    return(
+    <>
+    <SignUpForm />
+      <h3> Login instead? <button onClick={() => setView(_ => registration.LOGIN)}>Login</button>:</h3>
+    </>
+    )
+  }
 }
