c6014329484e4929d96f530c6906b392d9d8221e
Nathan Sierra <arreisnahtan@gmail.com>
2021-08-24 20:51:40 -0400

    feat: delete works with resact query

--- pages/manageTasks.tsx
+++ pages/manageTasks.tsx
@@ -1,12 +1,10 @@
 import axios from 'axios'
-import { keyBy } from 'lodash'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import TaskForm, { initialTask } from '../common/components/Task/TaskForm/TaskForm'
 import { Itask } from '../common/components/Task/TaskForm/Task'
 import { TaskList } from '../common/components/Task/TaskList/TaskList'
 
-import { v4 as uuidv4 } from 'uuid';
-import { api, BASE_HREF } from '../common/api'
+import { api } from '../common/api'
 import { useAuth } from '../common/contexts/authContext'
 import { useMutation, useQuery, useQueryClient } from 'react-query'
 
@@ -23,7 +21,6 @@ export const getAccountsByTeamID = async (teamID: string) => {
 }
 
 export default function ManageTasks (props: Iprops) {
-  const [taskListByID, setTaskListByID] = useState({[initialTask.id]: initialTask});
   const [selectedTask, setSelectedTask] = useState(initialTask);
   const { user, team } = useAuth();
 
@@ -66,6 +63,24 @@ export default function ManageTasks (props: Iprops) {
     }
   )
 
+  const deleteTaskMutation = (useMutation(
+    (deletedTaskID: string) => axios.delete(`${api.TASK}/${deletedTaskID}`),
+    {
+      onMutate: deletedTaskID => {
+        const previousTasks = queryClient.getQueryData<Itask[]>('tasks');
+        const removeDeleted = previousTasks?.filter((task) => task.id !== deletedTaskID);
+
+        queryClient.setQueryData('tasks', removeDeleted);
+
+        return queryClient.setQueryData('tasks', previousTasks);
+      },
+
+            // Always refetch after error or success:
+      onSettled: () => {
+        queryClient.invalidateQueries('tasks')
+      },
+    }))
+
 
 
   const { isLoading: isTasksLoading, data: taskData } = useQuery(['tasks', teamID], async () => { return getTasksByTeamID(teamID)})
@@ -77,27 +92,12 @@ export default function ManageTasks (props: Iprops) {
     return <h1>Loading</h1>
   }
 
-  const deleteTask = (id: string) => {
-    const {[id]: deletedTask, ...tasksByID} = taskListByID;
-    const deleteTask = async () => {
-      try {
-      const result = await axios.delete(`${api.TASK}/${id}`);
-      }
-      catch(e) {
-        console.info(e);
-      }
-    }
-
-    deleteTask();
-    setTaskListByID(tasksByID)
-  }
-
     return (
       <>
         <h1>Team: { user && user.teams && user.teams[0].name } </h1>
         <div className="m-4 flex flex-col justify-center items-center">
         <TaskForm task={selectedTask} saveTask={addTodoMutation.mutate} users={userData.accounts} color="hsla(50, 100%, 63%, 1)"/>
-        <TaskList setSelectedTask={setSelectedTask} onDelete={deleteTask} tasks={Object.values(taskData)} />
+        <TaskList setSelectedTask={setSelectedTask} onDelete={deleteTaskMutation.mutate} tasks={Object.values(taskData)} />
         </div>
         </>
     )
